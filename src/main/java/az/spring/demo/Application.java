0d14d423bcd78d1acfa2b7e5e142b5471da8e297
Yasin Sadiqzade <86802704+Matrix0101y@users.noreply.github.com>
2022-08-04 14:36:38 +0400

    My project

--- src/main/java/az/spring/demo/Application.java
+++ src/main/java/az/spring/demo/Application.java
@@ -1,9 +1,7 @@
 package az.spring.demo;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
